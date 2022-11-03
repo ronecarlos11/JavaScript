f5a37222c4aa5b28097be8d9e4a0f8bbbef3e270
ronecarlos11 <rone.carlos11@gmail.com>
2022-11-03 17:44:27 -0300

    condicoes

---
+++ ex008.js
@@ -0,0 +1,9 @@
+var vel = 59
+console.log(`A velocidade do seu carro é 60.5km/h`)
+if (vel > 60) { //Exemplo de condição simples - Para criar um comentario em JS usar //
+        console.log(`Você ultrapassou a velocidade pertimita. Multado!`)
+
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
+
+ 
---
+++ ex009.js
@@ -0,0 +1,7 @@
+var país = 'EUA' //Escolha o país de origem//
+console.log(`Vivendo em ${país}`) //Display o vivendo em "país"
+if (país == 'Brasil') {
+    console.log('Você é Brasileiro')
+} else { //Exemplo de condição composta //
+    console.log('Você é Estrangeiro')
+}
